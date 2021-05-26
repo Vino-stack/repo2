2fad8f7a0cb554cf2f6e1ff8abcbbb5bcb9cfe18
Vino-stack <66718873+Vino-stack@users.noreply.github.com>
2021-05-26 12:27:25 +0530

    Delete install.sh

--- Redis-folder/install.sh
+++
@@ -1,5 +0,0 @@
-# Connecting to the Redis instance from MIG( should located in same region and network as the Redis instance.)
-  
-  Install telnet using apt-get:
-
-      sudo apt-get install telnet
