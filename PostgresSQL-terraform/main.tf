9a50478a48e6bfc70b32c0e3b485bdb672234486
Vino-stack <66718873+Vino-stack@users.noreply.github.com>
2021-05-29 10:23:13 +0530

    Update main.tf

--- PostgresSQL-terraform/main.tf
+++ PostgresSQL-terraform/main.tf
@@ -4,3 +4,14 @@ provider "google"{
   region =var.region
   zone =var.zone
 }
+
+
+
+
+
+
+https://github.com/terraform-google-modules/terraform-google-sql-db/blob/master/modules/postgresql/main.tf
+
+https://gmusumeci.medium.com/getting-started-with-terraform-and-google-cloud-platform-gcp-deploying-a-postgresql-database-ce1664fb038c
+
+https://github.com/terraform-google-modules/terraform-google-sql-db/blob/master/examples/postgresql-ha/main.tf
