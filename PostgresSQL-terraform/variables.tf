63c1a5a842afbd6a3cbf8085088e7bd790f3958c
Vino-stack <66718873+Vino-stack@users.noreply.github.com>
2021-05-29 13:29:07 +0530

    Create variables.tf

---
+++ PostgresSQL-terraform/variables.tf
@@ -0,0 +1,10 @@
+# define GCP project name
+variable "app_project" {
+  type = string
+  description = "GCP project name"
+}
+# define GCP region
+variable "gcp_region_1" {
+  type = string
+  description = "GCP region"
+}
