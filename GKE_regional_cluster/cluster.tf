105cc2f0c60cc626dc023b03375d95670f825047
Vino-stack <66718873+Vino-stack@users.noreply.github.com>
2021-06-19 16:32:02 +0530

    Update cluster.tf

--- GKE_regional_cluster/cluster.tf
+++ GKE_regional_cluster/cluster.tf
@@ -1,13 +1,27 @@
-provider "google"{
-  project =var.gcp_project_id
-  region =var.region
-  zone =var.zone
+
+data "google_client_config" "provider" {}
+
+/* data "google_container_cluster" "my_cluster" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+  project= var.gcp_project_id
+} */
+
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.primary.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+  )
 }
+
+
 
 # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
 resource "google_service_account" "default" {
   account_id   = "vino-terraform-sa"
   display_name = "Service Account"
+  project=var.gcp_project_id
 }
 
 resource "google_container_cluster" "primary" {
@@ -18,7 +32,7 @@ resource "google_container_cluster" "primary" {
   depends_on    = [google_compute_network.vpc-net]
   network = google_compute_network.vpc-net.id
   subnetwork = google_compute_subnetwork.vpc-subnet.id
-
+  project=var.gcp_project_id
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
@@ -26,7 +40,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   location   = "us-central1"
   cluster    = google_container_cluster.primary.name
   node_count = 1
-
+project=var.gcp_project_id
   node_config {
     preemptible  = true
     machine_type = var.vm-machine_type
@@ -45,6 +59,6 @@ module "my-app-workload-identity" {
   source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   name       = "my-application-name"
   namespace  = "default"
-  project_id = "my-gcp-project-name"
-  roles = ["roles/storage.Admin", "roles/compute.Admin"]
+  project_id =var.gcp_project_id
+  //roles = ["roles/storage.Admin", "roles/compute.Admin"]
 }
